77895c11d2605633c2058a806988f4cf6a46429a
balajipachai <balaji.pachai08@gmail.com>
2021-10-22 03:16:09 +0530

    feat: updates pancake router testnet address since PTP uses fees
    * deployes contract on bsc testnet 0xE5E9e22a704FEC501C8981a81180AE568C867a6a

--- contracts/PlayTimeProfit.sol
+++ contracts/PlayTimeProfit.sol
@@ -105,7 +105,8 @@ contract PlayTimeProfit is ERC20, Ownable {
 
         liquidityWallet = owner();
 
-        // Pancake Router Testnet: 0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3
+        // Pancake Router Testnet: 0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3 (Router for tokens that DO NOT TAKE FEES)
+        // Pancake Router Testnet: 0xD99D1c33F9fC3444f8101754aBC46c52416550D1 (Router for tokens that take fees)
         // Pancake Router Mainnet: 0x10ED43C718714eb63d5aA57B78B54704E256024E
         IPancakeRouter02 _pancakeV2Router = IPancakeRouter02(
             0x10ED43C718714eb63d5aA57B78B54704E256024E
